ea625150d7069ff7b549adcff08bc8884eba9132
Kuba9028719 <9028719@student.tcrmbo.nl>
2026-07-22 20:31:11 +0200

    fix: add error logging to RedisMessageListener and bump version to 1.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "dev.kubister11"
-version = "1.0.2"
+version = "1.0.3"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/dev/kubister11/messenger/impl/redis/RedisMessageListener.kt
+++ src/main/kotlin/dev/kubister11/messenger/impl/redis/RedisMessageListener.kt
@@ -1,13 +1,25 @@
 package dev.kubister11.messenger.impl.redis
 
 import dev.kubister11.messenger.listener.MessageListener
+import org.slf4j.LoggerFactory
 import java.util.concurrent.Executor
 
 class RedisMessageListener<T : Any>(
     val listener: MessageListener<T>,
     private val executor: Executor
 ) : org.redisson.api.listener.MessageListener<T> {
+
+    private companion object {
+        private val logger = LoggerFactory.getLogger(RedisMessageListener::class.java)
+    }
+
     override fun onMessage(channel: CharSequence, msg: T) {
-        this.executor.execute { this.listener.onMessage(msg) }
+        this.executor.execute {
+            try {
+                this.listener.onMessage(msg)
+            } catch (throwable: Throwable) {
+                logger.error("Listener on channel {} threw", channel, throwable)
+            }
+        }
     }
 }
